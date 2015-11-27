a16fe091c84cb49c0599544db9ace25b0231e1b5
Allen Phelps <allenatmc@gmail.com>
2015-11-27 09:11:34 -0600

    mvp for FB, in need of debugging

--- js/ctrls/mainViewCtrl.js
+++ js/ctrls/mainViewCtrl.js
@@ -2,12 +2,8 @@ app.controller("MainViewCtrl",
   ["$routeParams", "$location", "dataAccess",
   function($routeParams, $location, $dataAccess) {
 
-
     this.pics = $dataAccess.getPhotosAsArray();
-
   	this.currentUser = $dataAccess.getUser();
-
-
 
 
 }]);
