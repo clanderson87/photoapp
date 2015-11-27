9a7cb3ac05defa8003496258a97b47485bb6f18f
Allen Phelps <allenatmc@gmail.com>
2015-11-27 10:38:52 -0600

    fix display issue for non-logged in users

--- js/ctrls/mainViewCtrl.js
+++ js/ctrls/mainViewCtrl.js
@@ -1,9 +1,21 @@
 app.controller("MainViewCtrl",
-  ["$routeParams", "$location", "dataAccess",
-  function($routeParams, $location, $dataAccess) {
+  ["$routeParams", "$location", "dataAccess", "$rootScope",
+  function($routeParams, $location, $dataAccess, $rootScope) {
 
     this.pics = $dataAccess.getPhotosAsArray();
   	this.currentUser = $dataAccess.getUser();
 
+  	this.checkUid = function(pic, index, array) {
+  			console.log(this.currentUser);
+	  		if (this.currentUser) {
+  			return pic.uid != this.currentUser.uid;
+  		} else {
+  			return true;
+  		}
+ 	 };
+
+ 	$rootScope.$on('$routeChangeSuccess', function () {
+  		this.checkUid();
+	}.bind(this));
 
 }]);
